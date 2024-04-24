2edb5760ef2fbcd86b418be178950bab454452da
Molly Gregson <mollykg123@msn.com>
2024-04-24 10:37:02 +0100

    favicon moved to public

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react'
 
 import star from './assets/Star.png'
 import wars from './assets/Wars.png'
-import sound from './assets/sounds/main-theme.mp3'
+// import sound from './assets/sounds/main-theme.mp3'
 
 export default function Home() {
 
@@ -15,24 +15,25 @@ export default function Home() {
   const handleAnimationEnd = () => {
     setAnimationComplete(true)
   }
-  useEffect(() => {
-    const audio = new Audio(sound);
-    audio.loop = true; // Loop the sound
 
-    const playAudio = () => {
-      audio.play();
-    };
+  // useEffect(() => {
+  //   const audio = new Audio(sound);
+  //   audio.loop = true; // Loop the sound
 
-    // Wait for the audio to be fully loaded before playing
-    audio.addEventListener('canplaythrough', playAudio);
+  //   const playAudio = () => {
+  //     audio.play();
+  //   };
 
-    // Clean up the event listener when the component unmounts
-    return () => {
-      audio.removeEventListener('canplaythrough', playAudio);
-      audio.pause();
-      audio.currentTime = 0;
-    };
-  }, []); // Empty dependency array to run the effect only once
+  //   // Wait for the audio to be fully loaded before playing
+  //   audio.addEventListener('canplaythrough', playAudio);
+
+  //   // Clean up the event listener when the component unmounts
+  //   return () => {
+  //     audio.removeEventListener('canplaythrough', playAudio);
+  //     audio.pause();
+  //     audio.currentTime = 0;
+  //   };
+  // }, []); // Empty dependency array to run the effect only once
 
   return (
     <container className="animation-container">
